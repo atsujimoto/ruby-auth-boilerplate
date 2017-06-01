2299aea0f70f082048352cec0ffd78103b088929
jigglor <andrewtsuji@gmail.com>
2017-06-01 10:31:22 -0700

    working facebook auth

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'main#index'
+
+  get 'auth/logout' => 'auth#logout'
+  get 'auth/failure' => 'auth#failure'
+  get 'auth/:provider/callback' => 'auth#callback'
+end
